65f1d71f363ade78584bcc4cfc450f3904a09bcf
guzal <220930@astanait.edu.kz>
2023-05-12 20:16:57 +0600

    Step 11: I created new class MyTestingClass and put here objects, created MyTestingClass() method where initialize id and name.

---
+++ src/MyTestingClass.java
@@ -0,0 +1,8 @@
+public class MyTestingClass {
+    private int id; // private integer to store the id of the object
+    private String name; // private string to store the name of the object
+    public MyTestingClass(int id, String name) {
+        this.id = id; // constructor that initializes the id field with the passed value
+        this.name = name; // constructor that initializes the name field with the passed value
+    }
+}
