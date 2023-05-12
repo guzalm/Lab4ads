e6571e702d59a5f784be1d0cbd898dc7ba7827b4
guzal <220930@astanait.edu.kz>
2023-05-12 17:17:36 +0600

    Step 5: First created function called hash()

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -1,5 +1,6 @@
 public class MyHashTable<K, V> {
 
+    // Inner class to represent a node in the linked list
     private class HashNode<K, V> {
         K key;
         V value;
@@ -12,15 +13,24 @@ public class MyHashTable<K, V> {
         }
     }
 
+    // The array to hold the table of nodes
     private HashNode<K, V>[] table;
+    // The number of elements in the table
     private int size;
 
+    // Default constructor that creates a table with 16 buckets
     public MyHashTable() {
         this(16);
     }
 
+    // Constructor that creates a table with the specified number of buckets
     public MyHashTable(int M) {
         table = new HashNode[M];
         size = 0;
+    }
+    // Hash function that returns an index into the table for a given key
+    private int hash(K key) {
+        int hashCode = key.hashCode() % table.length;
+        return (hashCode < 0) ? hashCode + table.length : hashCode;
     }
 }
