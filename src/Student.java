395bb4a955f891d008082f706ae60c905b4c11df
guzal <220930@astanait.edu.kz>
2023-05-14 14:55:53 +0600

    Step 16:Full created Student class

--- src/Student.java
+++ src/Student.java
@@ -1,26 +1,31 @@
 public class Student {
+
+    // Fields to store the name and ID of the student
     private String name;
     private int id;
-    // other fields and methods
 
+    // Constructor to initialize the name and ID of the student
     public Student(String name, int id) {
         this.name = name;
         this.id = id;
     }
 
-    // getters and setters for name and id
+    // Getter method for the name field
     public String getName() {
         return name;
     }
 
+    // Setter method for the name field
     public void setName(String name) {
         this.name = name;
     }
 
+    // Getter method for the id field
     public int getId() {
         return id;
     }
 
+    // Setter method for the id field
     public void setId(int id) {
         this.id = id;
     }
